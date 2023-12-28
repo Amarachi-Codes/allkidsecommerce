a095c13a0050a70eb5f2d191ff75fdece04994c0
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-28 20:37:46 +0100

    updated id

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const vendorname = document.getElementById("vendorname").value.trim()
 const image = document.getElementById("img").value
 
 if(productname && productprice && vendorname && image){
+    const product ={
+        id:generateId(),
+        pName: productname,
+        price: productprice,
+        vName:vendorname,
+        img:image
+    }
     alert("product sucessfully added")
 }else{
     alert("please enter product details")
@@ -14,3 +21,7 @@ if(productname && productprice && vendorname && image){
 }
 const form = document.getElementById("form")
 form.addEventListener("submit", addProduct)
+
+function generateId(){
+    return "^"+ Math.random().toString(36).substring(2, 9);
+}
