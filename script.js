275d8b36a4c57e7ef37ab118391e2bedb1475c36
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-29 17:10:00 +0100

    updated

--- script.js
+++ script.js
@@ -17,15 +17,39 @@ if(productname && productprice && vendorname && image){
   let products = JSON.parse(localStorage.getItem("products"))||[]
   products.push(product)
   localStorage.setItem("products", JSON.stringify(products));
+  document.getElementById("form").reset();
     alert("product sucessfully added")
+    displayProducts();
 }else{
     alert("please enter product details")
 }
 }
 const form = document.getElementById("form")
-form.addEventListener("submit", addProduct)
+form.addEventListener("submit", addProduct);
 
 function generateId(){
     return "^"+ Math.random().toString(36).substring(2, 9);
 }
 
+function displayProducts(){
+    const card = document.getElementById("card");
+    card.innerHTML="";
+    const products = JSON.parse(localStorage.getItem("products"))||[]
+
+    //create p items
+    products.forEach((product) => {
+        const divtags = document.createElement("div");
+        
+        divtags.innerHTML =`
+        <img src=${product.img}<br>
+        <p>${product.pName}</p>
+        <p>${product.price}</p>
+        <p>${product.vName}</p>
+
+        `;
+        card.appendChild(divtags);
+        
+    });
+    
+}
+displayProducts()
