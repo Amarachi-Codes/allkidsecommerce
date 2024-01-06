1b3ad41d283f0c6ab85a73852451971b0ec19cea
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-06 18:41:30 +0100

    updated

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 
-function addProduct(event){ 
+function addProduct(event) {
     event.preventDefault()
-const productname = document.getElementById("productname").value.trim().toUpperCase()
-const productprice = document.getElementById("productprice").value.trim()
-const vendorname = document.getElementById("vendorname").value.trim().toLowerCase()
-const image = document.getElementById("img").value
+    const productname = document.getElementById("productname").value.trim().toUpperCase()
+    const productprice = document.getElementById("productprice").value.trim()
+    const vendorname = document.getElementById("vendorname").value.trim().toLowerCase()
+    const image = document.getElementById("img").value
 
-if(productname && productprice && vendorname && image){
-    const product ={
-        id:generateId(),
-        pName: productname,
-        price: "N" + productprice,
-        vName:vendorname,
-        img:image
+    if (productname && productprice && vendorname && image) {
+        const product = {
+            id: generateId(),
+            pName: productname,
+            price: "N" + productprice,
+            vName: vendorname,
+            img: image
+        }
+        let products = JSON.parse(localStorage.getItem("products")) || []
+        products.push(product)
+        localStorage.setItem("products", JSON.stringify(products));
+        document.getElementById("form").reset();
+        alert("product sucessfully added")
+        displayProducts();
+    } else {
+        alert("please enter product details")
     }
-  let products = JSON.parse(localStorage.getItem("products"))||[]
-  products.push(product)
-  localStorage.setItem("products", JSON.stringify(products));
-  document.getElementById("form").reset();
-    alert("product sucessfully added")
-    displayProducts();
-}else{
-    alert("please enter product details")
-}
 
 
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("form");
     form.addEventListener("submit", addProduct);
     displayProducts();
 });
-  
 
 
-function generateId(){
-    return "^"+ Math.random().toString(36).substring(2, 9);
+
+function generateId() {
+    return "^" + Math.random().toString(36).substring(2, 9);
 }
 
 
 
 
-function displayProducts(){
-    const card = document.getElementById("card");
-    card.innerHTML="";
-    const products = JSON.parse(localStorage.getItem("products"))||[]
+function displayProducts() {
+    const card = document.getElementById("#card");
+    card.innerHTML = "";
+    const products = JSON.parse(localStorage.getItem("products")) || []
 
     //create p items
     products.forEach((product) => {
         const divtags = document.createElement("div");
-        
-        divtags.innerHTML =`
+
+        divtags.innerHTML = `
         <img src=${product.img}<br>
         <p>${product.pName}</p>
         <p>${product.price}</p>
@@ -58,8 +58,59 @@ function displayProducts(){
 
         `;
         card.appendChild(divtags);
-        
+
     });
-    
+
 }
 displayProducts()
+
+
+// login validation
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("#user_form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        validateForm()
+    })
+})
+
+function validateForm() {
+    isValid = true;
+    if (!validateUsername) {
+        isValid = false
+    }
+    if (!validatePassword) {
+        isValid = false
+    }
+    if (isValid) {
+        alert("form successfully submitted")
+    }
+}
+
+
+function validateUsername() {
+    const username = document.getElementById("#username");
+    const username_error = document.getElementById("#username_error")
+    if (username === "") {
+        username_error.textContent = "Username cannot be empty "
+        return false
+    } else {
+        username_error.textContent = ""
+        return true
+    }
+
+}
+
+function validatePassword() {
+    const password = document.getElementById("#password");
+    const password_error = document.getElementById("#password_error");
+    if (password < 5) {
+        password_error.textContent = "Password length must be greater than 5 characters"
+        return false
+    } else {
+        password_error.textContent = ""
+        return true
+    }
+
+}
+
